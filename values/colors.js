c6fc97bb30df7f70c8acd584fbdc55c9d0ed5d52
hayyaun <hayyanhami@outlook.com>
2021-12-23 23:47:56 +0330

    add init files and package struct

--- values/colors.js
+++ values/colors.js
@@ -1,3 +1,22 @@
-const colors = {};
+const colors = {
+  accent: "#deb887",
+  primary: "#318161",
+  secondary: "#fdebd2",
+  background: "#f1f2f2",
+  // by variant
+  info: "#006bff",
+  error: "#e63946",
+  warning: "#e67f0d",
+  success: "#4caf50",
+  successLight: "#4bdc52",
+  disable: "#8d918f",
+  primaryDisable: "#92d6ba",
+  // by usage
+  badge: "#ffde26",
+  // by name
+  black: "#040c09",
+  grey: "#bbbdbc",
+  greyLight: "#e8e9e9",
+};
 
 export default colors;
---
+++ values/index.js
@@ -0,0 +1,5 @@
+export { default as colors } from "./colors";
+export { default as layout } from "./layout";
+export { default as quantities } from "./quantities";
+export { default as strings } from "./strings";
+export * from "./enums";
